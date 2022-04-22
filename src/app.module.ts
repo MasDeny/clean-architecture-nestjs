3e52cf902885fb784c1da0ec72d631d054e6062a
HOHO <kickdeny@gmail.com>
2022-04-22 08:47:39 +0700

    add exceptions error, debug, etc on structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { TypeormConfigModule } from './infrastructure/config/typeorm/typeorm.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeormConfigModule, LoggerModule],
+  imports: [
+    EnvironmentConfigModule,
+    TypeormConfigModule,
+    LoggerModule,
+    ExceptionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
